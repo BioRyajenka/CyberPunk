2b5358902e17030f5e2f2fad4d0a1a7ec0924156
BioRyajenka <igor-sushencev@yandex.ru>
2015-11-18 00:03:57 +0300

    Jar and readme added. Also minor fixes.

--- src/com/jackson/cyberpunk/Inventory.java
+++ src/com/jackson/cyberpunk/Inventory.java
@@ -9,118 +9,118 @@ import com.jackson.myengine.Log;
 import com.jackson.myengine.Utils;
 
 public class Inventory {
-    private Knapsack knapsack;
-    private LinkedList<Item> items;
+	private Knapsack knapsack;
+	private LinkedList<Item> items;
 
-    public Inventory(Knapsack knapsack) {
-	this.knapsack = knapsack;
-	items = new LinkedList<Item>();
-    }
-
-    public boolean canAdd(Item item) {
-	int n = knapsack.getCapacity() / 5, m = 5, 
-	    w = item.getSizeJ(), h = item.getSizeI();
-	
-	if (item instanceof CountableItem) {
-	    CountableItem сi = (CountableItem) item;
-	    int sum = сi.getAmount();
-	    for (Item e : items)
-		if (item.getClass().equals(e.getClass())) {
-		    //here we are restocking
-		    CountableItem ce = (CountableItem) e;
-		    sum += ce.getAmount();
-		    sum = Math.max(0, sum - ce.getStackSize());
-		    if (sum == 0) {
-			return true;
-		    }
-		}
-	    // sum > 0
+	public Inventory(Knapsack knapsack) {
+		this.knapsack = knapsack;
+		items = new LinkedList<Item>();
 	}
 
-	for (int i = 0; i < n - h + 1; i++)
-	    for (int j = 0; j < m - w + 1; j++) {
-		boolean ok = true;
-		for (Item e : items) {
-		    ok &= (!Utils.boundingBox(i, j, i + h - 1, j + w - 1,
-			    e.getI(), e.getJ(), e.getI() + e.getSizeI() - 1,
-			    e.getJ() + e.getSizeJ() - 1));
-		}
-		if (ok) {
-		    return true;
-		}
-	    }
-	return false;
-    }
+	public boolean canAdd(Item item) {
+		int n = knapsack.getCapacity() / 5, m = 5, w = item.getSizeJ(), h = item
+				.getSizeI();
 
-    public void add(Item item) {
-	if (!canAdd(item)) {
-	    Log.e("Trying to add smth in full inventory!");
-	    // TODO: if cant, drop
-	    return;
+		if (item instanceof CountableItem) {
+			CountableItem i = (CountableItem) item;
+			int sum = i.getAmount();
+			for (Item e : items)
+				if (item.getClass().equals(e.getClass())) {
+					// here we are restocking
+					CountableItem ce = (CountableItem) e;
+					sum += ce.getAmount();
+					sum = Math.max(0, sum - ce.getStackSize());
+					if (sum == 0) {
+						return true;
+					}
+				}
+			// sum > 0
+		}
+
+		for (int i = 0; i < n - h + 1; i++)
+			for (int j = 0; j < m - w + 1; j++) {
+				boolean ok = true;
+				for (Item e : items) {
+					ok &= (!Utils.boundingBox(i, j, i + h - 1, j + w - 1, e.getI(), e
+							.getJ(), e.getI() + e.getSizeI() - 1, e.getJ() + e.getSizeJ()
+									- 1));
+				}
+				if (ok) {
+					return true;
+				}
+			}
+		return false;
 	}
-	int n = knapsack.getCapacity() / 5, m = 5, w = item.getSizeJ(), h = item
-		.getSizeI(), i1 = -1, j1 = -1;
 
-	if (item instanceof CountableItem) {
-	    CountableItem ci = (CountableItem) item;
-	    for (Item e : items)
-		if (item.getClass().equals(e.getClass())) {
-		    CountableItem ai = (CountableItem) e;
-		    int sum = ai.getAmount() + ci.getAmount();
-		    ai.setAmount(Math.min(sum, ci.getStackSize()));
-		    sum = Math.max(0, sum - ci.getStackSize());
-		    ci.setAmount(sum);
-		    if (sum == 0)
+	public void add(Item item) {
+		if (!canAdd(item)) {
+			Log.e("Trying to add smth in full inventory!");
+			// TODO: if cant, drop
 			return;
 		}
-	}
+		int n = knapsack.getCapacity() / 5, m = 5, w = item.getSizeJ(), h = item
+				.getSizeI(), i1 = -1, j1 = -1;
 
-	// ищем подходящее место
-	for1: for (int i = 0; i < n - h + 1; i++) {
-	    for (int j = 0; j < m - w + 1; j++) {
-		boolean ok = true;
-		for (Item e : items)
-		    ok &= (!Utils.boundingBox(i, j, i + h - 1, j + w - 1,
-			    e.getI(), e.getJ(), e.getI() + e.getSizeI() - 1,
-			    e.getJ() + e.getSizeJ() - 1));
-		if (ok) {
-		    i1 = i;
-		    j1 = j;
-		    break for1;
+		if (item instanceof CountableItem) {
+			CountableItem ci = (CountableItem) item;
+			for (Item e : items)
+				if (item.getClass().equals(e.getClass())) {
+					CountableItem ai = (CountableItem) e;
+					int sum = ai.getAmount() + ci.getAmount();
+					ai.setAmount(Math.min(sum, ci.getStackSize()));
+					sum = Math.max(0, sum - ci.getStackSize());
+					ci.setAmount(sum);
+					if (sum == 0)
+						return;
+				}
 		}
-	    }
+
+		// пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+		for1: for (int i = 0; i < n - h + 1; i++) {
+			for (int j = 0; j < m - w + 1; j++) {
+				boolean ok = true;
+				for (Item e : items)
+					ok &= (!Utils.boundingBox(i, j, i + h - 1, j + w - 1, e.getI(), e
+							.getJ(), e.getI() + e.getSizeI() - 1, e.getJ() + e.getSizeJ()
+									- 1));
+				if (ok) {
+					i1 = i;
+					j1 = j;
+					break for1;
+				}
+			}
+		}
+		if (i1 == -1)
+			Log.e("Inventory.java: smth wrong...");
+		item.setIJ(i1, j1);
+		items.add(item);
 	}
-	if (i1 == -1)
-	    Log.e("Inventory.java: smth wrong...");
-	item.setIJ(i1, j1);
-	items.add(item);
-    }
 
-    public void remove(Item item) {
-	if (!items.contains(item)) {
-	    Log.e("Trying to remove smth which isn't exists!");
-	    return;
+	public void remove(Item item) {
+		if (!items.contains(item)) {
+			Log.e("Trying to remove smth which isn't exists!");
+			return;
+		}
+		items.remove(item);
 	}
-	items.remove(item);
-    }
 
-    public LinkedList<Item> getItems() {
-	return items;
-    }
+	public LinkedList<Item> getItems() {
+		return items;
+	}
 
-    public float getWeight() {
-	float weight = knapsack.getWeight();
-	for (Item i : items)
-	    weight += i.getWeight();
-	return weight;
-    }
+	public float getWeight() {
+		float weight = knapsack.getWeight();
+		for (Item i : items)
+			weight += i.getWeight();
+		return weight;
+	}
 
-    public Knapsack getKnapsack() {
-	return knapsack;
-    }
+	public Knapsack getKnapsack() {
+		return knapsack;
+	}
 
-    public void setKnapsack(Knapsack knapsack) {
-	this.knapsack = knapsack;
-	// TODO: replace items
-    }
+	public void setKnapsack(Knapsack knapsack) {
+		this.knapsack = knapsack;
+		// TODO: replace items
+	}
 }
--- src/com/jackson/myengine/FastSprite.java
+++ src/com/jackson/myengine/FastSprite.java
@@ -7,77 +7,78 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class FastSprite extends Entity {
-    private static Map<String, Image> initImages = new HashMap<String, Image>();
-    //allows to spend less memory, but no time. so, it's not neccesary 
-    //private static Set<Image> imageSet = new HashSet<Image>(); 
-    
-    //TODO: hashfunction, that considers size and color with default opportunity
+	private static Map<String, Image> initImages = new HashMap<String, Image>();
+	// allows to spend less memory, but no time. so, it's not neccesary
+	// private static Set<Image> imageSet = new HashSet<Image>();
 
-    private Image mImage;
-    private float initialWidth, initialHeight;
+	// TODO: hashfunction, that considers size and color with default
+	// opportunity
 
-    public FastSprite(float pX, float pY, String path) {
-	super(pX, pY);
-	setImage(path);
-	initialWidth = getWidth();
-	initialHeight = getHeight();
-    }
+	private Image mImage;
+	private float initialWidth, initialHeight;
 
-    @Override
-    public void setColor(float pRed, float pGreen, float pBlue, float pAlpha) {
-	super.setColor(pRed, pGreen, pBlue, pAlpha);
-	mImage.getFlippedCopy(false, false);
-	mImage.setImageColor(pRed, pGreen, pBlue, pAlpha);
-    }
-
-    public float getWidth() {
-	return mImage.getWidth();
-    }
-
-    public float getHeight() {
-	return mImage.getHeight();
-    }
-
-    public Image getImage() {
-	return mImage;
-    }
-
-    public void setImage(String path) {
-	Image image = null;
-	if (initImages.containsKey(path)) {
-	    image = initImages.get(path);
-	} else {
-	    try {
-		image = new Image(path);
-		initImages.put(path, image);
-	    } catch (SlickException e) {
-		Log.e(e.toString());
-	    }
+	public FastSprite(float pX, float pY, String path) {
+		super(pX, pY);
+		setImage(path);
+		initialWidth = getWidth();
+		initialHeight = getHeight();
 	}
-	setImage(image);
-    }
 
-    private void setImage(Image pImage) {
-	this.mImage = pImage;
-	mImage.setImageColor(getRed(), getGreen(), getBlue(), getAlpha());
-    }
+	@Override
+	public void setColor(float pRed, float pGreen, float pBlue, float pAlpha) {
+		super.setColor(pRed, pGreen, pBlue, pAlpha);
+		mImage.getFlippedCopy(false, false);
+		mImage.setImageColor(pRed, pGreen, pBlue, pAlpha);
+	}
 
-    public void onDraw() {
-	mImage.draw(getGlobalX(), getGlobalY());
-	super.onDraw();
-    }
+	public float getWidth() {
+		return mImage.getWidth();
+	}
 
-    public void setSize(float pWidth, float pHeight) {
-	mImage = mImage.getScaledCopy((int) pWidth, (int) pHeight);
-	setColor(mRed, mGreen, mBlue, mAlpha);
-    }
+	public float getHeight() {
+		return mImage.getHeight();
+	}
 
-    public void restoreSize() {
-	setSize(initialWidth, initialHeight);
-    }
+	public Image getImage() {
+		return mImage;
+	}
 
-    public void flipHorizontally() {
-	mImage = mImage.getFlippedCopy(true, false);
-	setColor(mRed, mGreen, mBlue, mAlpha);
-    }
+	public void setImage(String path) {
+		Image image = null;
+		if (initImages.containsKey(path)) {
+			image = initImages.get(path);
+		} else {
+			try {
+				image = new Image(path);
+				initImages.put(path, image);
+			} catch (SlickException e) {
+				Log.e(e.toString());
+			}
+		}
+		setImage(image);
+	}
+
+	private void setImage(Image pImage) {
+		this.mImage = pImage;
+		mImage.setImageColor(getRed(), getGreen(), getBlue(), getAlpha());
+	}
+
+	public void onDraw() {
+		mImage.draw(getGlobalX(), getGlobalY());
+		super.onDraw();
+	}
+
+	public void setSize(float pWidth, float pHeight) {
+		mImage = mImage.getScaledCopy((int) pWidth, (int) pHeight);
+		setColor(mRed, mGreen, mBlue, mAlpha);
+	}
+
+	public void restoreSize() {
+		setSize(initialWidth, initialHeight);
+	}
+
+	public void flipHorizontally() {
+		mImage = mImage.getFlippedCopy(true, false);
+		setColor(mRed, mGreen, mBlue, mAlpha);
+	}
 }
