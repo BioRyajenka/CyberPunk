f696c44e2eae80a9e4d5f1e644e985ce3d1aae4b
BioRyajenka <igor-sushencev@yandex.ru>
2016-03-14 15:51:53 +0300

    binary added

--- build.bat
+++
@@ -1,9 +0,0 @@
-#for /r src %i in (*.java) do javac %i -sourcepath src -d bin
-#javac src\\*.java -d bin
-
-dir *.java /s /b > FilesList.txt
-mkdir bin
-javac -cp ".\lib\lwjgl_natives\windows\lwjgl.dll;.\lib\lwjgl_natives\windows\lwjgl64.dll;.\lib\slick2d_jars\*" -d .\bin @FilesList.txt
-del FilesList.txt
-
-#jar cvfe .\bin\game.jar com.jackson.cyberpunk.Game .\bin\* .\lib\lwjgl_natives\windows\lwjgl.dll;.\lib\lwjgl_natives\windows\lwjgl64.dll;.\lib\slick2d_jars\*
